1f8ab7f1c2e4278c540b75d6f42cc36f43af2357
Tanzebruh <tanzirhasanhasan@gmail.com>
2021-09-19 22:56:38 -0400

    added an angle changing feature and repaired a critical math error

--- Interactive Recursive Tree/sketch.js
+++ Interactive Recursive Tree/sketch.js
@@ -1,5 +1,7 @@
 var canv;
-var changeA = Math.PI/6;  
+var changeA = Math.PI*2/3;
+var col = 'red';
+var changeC = true; 
 function windowResized() {
     resizeCanvas(window.innerWidth*7/8, window.innerHeight*7/8);
   }
@@ -8,20 +10,21 @@ function windowResized() {
     background(54, 69, 79);
   }
   function draw (){
+    background(54, 69, 79);
     translate(width/2,height);
-    stroke ('red');
+    stroke (col);
     var distance = height*1/3;
     strokeWeight(3);
     line(0, height, 0, -distance);
     translate(0,-distance);
-    rotate(Math.PI/3)
-    recurse (distance*3/5, changeA, 5)
+    rotate(Math.PI/2 - changeA);//fixed critical math error
+    recurse (distance*3/5, changeA, 5);
+    
   }
 
-  //rewrite
   function recurse(distance, angle, iterations){
     if (iterations >0){
-      stroke ('pink');
+      stroke (col);
       push();
       var change = createVector(-distance*Math.cos(angle+changeA), -distance*Math.sin(angle+changeA));
       line (0,0,change.x, change.y);
@@ -35,31 +38,14 @@ function windowResized() {
       recurse (distance*3/5, angle-changeA, iterations-1);
       pop ();
     }
-
   }
-  /*
-    function recurse(distance, angle, iterations){
-    if (iterations >0){
-      stroke ('pink');
-      push();
-      
-      rotate(angle);
-      var change = createVector(distance.x, distance.y);
-      change = change.mult(.67);
-      line (0,0,change.x, change.y);
-      translate (change.x,change.y);
-      recurse (change, angle, iterations-1);
-      pop ();
-      push();
-      rotate(-angle);
-      distance = distance.mult(.67)      
-      line (0,0,distance.x, distance.y);
-      translate (distance.x,distance.y);
-      recurse (distance, -angle, iterations-1);
-      pop ();
-      console.log ('worked');
-    }
-    else{
-      console.log("done");
-    }
-  }*/
+function changeAngle(){
+  //implment soon
+}
+function changeCol(){
+  //implement next
+}
+
+function mouseDragged() {
+  changeA += .1;//proof of concept
+}
